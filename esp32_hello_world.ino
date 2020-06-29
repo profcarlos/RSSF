8ca9a273f8b16bbccd9a731fbb44188b855b5937
Carlos Silveira <31132270+profcarlos@users.noreply.github.com>
2020-06-29 17:57:48 -0300

    Update and rename esp32_e1_hello_world.ino to esp32_hello_world.ino

--- esp32_hello_world.ino
+++ esp32_hello_world.ino
@@ -2,7 +2,7 @@
   IFG Campus Goiânia
   Autor:    Carlos Silveira
   Objetivo: Programa de testes do ESP32. O LED interno pisca indefinidamente e escreve mensagem padrão.
-  Origem:   https://techtutorialsx.com/2017/04/24/esp32-connecting-to-a-wifi-network/
+  Origem:   https://techtutorialsx.com/2017/04/24/esp32-hello-world/
   URLs adicionais para Gerenciadores de Placas: https://dl.espressif.com/dl/package_esp32_index.json 
 *********/
 void setup() {
