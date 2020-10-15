723fb1e69d6558a74438ee3ce44f9ab0b7e8a36a
Carlos Silveira <31132270+profcarlos@users.noreply.github.com>
2020-10-15 17:47:35 -0300

    Create ESP8266_DHT11_webserver_refresh.ino

---
+++ ESP8266_DHT11_webserver_refresh.ino
@@ -0,0 +1,112 @@
+// Disponível em: https://lastminuteengineers.com/esp8266-dht11-dht22-web-server-tutorial/
+// Tabela de definições de pinos digitais
+// Disponível em: https://github.com/esp8266/Arduino/issues/584
+static const uint8_t D0   = 16;
+static const uint8_t D1   = 5;
+static const uint8_t D2   = 4;
+static const uint8_t D3   = 0;
+static const uint8_t D4   = 2;
+static const uint8_t D5   = 14;
+static const uint8_t D6   = 12;
+static const uint8_t D7   = 13;
+static const uint8_t D8   = 15;
+static const uint8_t D9   = 3;
+static const uint8_t D10  = 1;
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "DHT.h"
+
+// Uncomment one of the lines below for whatever DHT sensor type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+/*Put your SSID & Password*/
+const char* ssid = "Campos Oi Fibra 2G";
+// Insere a senha da Rede WiFi
+const char* password = "fdpcssm123";
+
+ESP8266WebServer server(80);
+
+// DHT Sensor
+uint8_t DHTPin = D1; 
+               
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);                
+
+float Temperature;
+float Humidity;
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  
+  pinMode(DHTPin, INPUT);
+
+  dht.begin();              
+
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, password);
+
+  //check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+}
+void loop() {
+  
+  server.handleClient();
+  
+}
+
+void handle_OnConnect() {
+
+ Temperature = dht.readTemperature(); // Gets the values of the temperature
+  Humidity = dht.readHumidity(); // Gets the values of the humidity 
+  server.send(200, "text/html", SendHTML(Temperature,Humidity)); 
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(float Temperaturestat,float Humiditystat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>ESP8266 Estacao Meteorologica </title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<div id=\"webpage\">\n";
+  ptr +="<h1>ESP8266 Estacao Meteorologica</h1>\n";
+  
+  ptr +="<p>Temperatura: ";
+  ptr +=(int)Temperaturestat;
+  ptr +=" Celsius</p>";
+  ptr +="<p>Umidade: ";
+  ptr +=(int)Humiditystat;
+  ptr +="%</p>";
+  
+  ptr +="</div>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
